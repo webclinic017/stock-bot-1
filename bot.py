6b21684b421f5f16d430790afdf7a29befac909d
Yodart <joaogabrielgcoutinho@gmail.com>
2020-09-28 04:12:31 -0300

    Send Messages

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os, sys, pika, requests, csv
+
+def callback(ch, method, properties, body):
+    data = body.decode("utf-8") 
+    stock_code=data.split("|")[0]
+    room = data.split("|")[1]
+    try:
+        with requests.Session() as s:
+            download = s.get('https://stooq.com/q/l/?s={}&f=sd2t2ohlcv&h&e=csv'.format(stock_code))
+            decoded = download.content.decode('utf-8')
+            rows = [x for x in csv.reader(decoded.splitlines(), delimiter=',')]
+            stock = rows[1][0]
+            quote = rows[1][3]
+            if quote == 'N/D':
+                requests.post('http://localhost:5000/message', json={"message":"Unable to find stock!","room": room,"username":"StockBot","secret":"jobsitystockbotkey"})
+            else:
+                requests.post('http://localhost:5000/message', json={"message":"{} quote is ${}".format(stock_code.upper(),quote),"room": room,"username":"StockBot","secret":"jobsitystockbotkey"})
+    except:
+        requests.post('http://localhost:5000/message', json={"message":"Error while fetching stock prices","room": room,"username":"StockBot","secret":"jobsitystockbotkey"})
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='stock-query')
+channel.basic_consume(queue='stock-query', on_message_callback=callback, auto_ack=True)
+channel.start_consuming()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/b/Desktop/jobsity-bot/venv"
+set -gx VIRTUAL_ENV "/Users/b/Documents/GitHub/jobsity-chat-bot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
